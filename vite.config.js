20bec77ffbbbff3dd477bd561d6cfbbfbe52f8bd
topoi <politopoi@protonmail.com>
2022-01-08 03:49:57 -0500

    add vite html minifier

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,23 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
-import { viteSingleFile } from "vite-plugin-singlefile"
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import { viteSingleFile } from "vite-plugin-singlefile";
+import { minifyHtml } from "vite-plugin-html";
 
 export default defineConfig({
-  plugins: [solidPlugin(), viteSingleFile()],
+  plugins: [solidPlugin(), viteSingleFile(), minifyHtml()],
   build: {
-    target: 'esnext',
+    target: "esnext",
     polyfillDynamicImport: false,
     //settings for singlefile
     assetsInlineLimit: 100000000,
-		chunkSizeWarningLimit: 100000000,
-		cssCodeSplit: false,
-		brotliSize: false,
-		rollupOptions: {
-			inlineDynamicImports: true,
-			output: {
-				manualChunks: () => "everything.js",
-			},
-		},
+    chunkSizeWarningLimit: 100000000,
+    cssCodeSplit: false,
+    brotliSize: false,
+    rollupOptions: {
+      inlineDynamicImports: true,
+      output: {
+        manualChunks: () => "everything.js",
+      },
+    },
   },
 });
